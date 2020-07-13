5f75d8fac56f76ad6ec365b79e347901ca5edbce
Anderson R <misterregisslv@gmail.com>
2020-07-13 18:12:32 -0300

    adicionado exe 1 com 5 programas(+,-,*,/,%)

---
+++ exercises/4_1/exe-js-1.js
@@ -0,0 +1,19 @@
+let a = 30;
+let b = 20;
+
+
+//programa de adição
+let add = a + b;
+console.log(add)
+//programa subtração
+let sub = a - b;
+console.log(sub)
+//programa de multiplicaçao
+let mult = a * b;
+console.log(mult)
+//programa de divisão
+let div = a / b;
+console.log(div)
+//programa de módulo
+let resto = a % b;
+console.log(resto)
