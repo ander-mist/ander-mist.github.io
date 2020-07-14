be5fb9ecc862baf7aa4efeacd2cdc3ad135403e4
Anderson R <misterregisslv@gmail.com>
2020-07-14 01:15:55 -0300

    adicionado exe 7 conversao de nota numeros por letra

---
+++ exercises/4_1/exe-js-07.js
@@ -0,0 +1,18 @@
+let note = 78;
+let note2 ="";
+
+if (note >= 90){
+    note2 = "A";
+}else if(note >= 80){
+    note2 = "B";
+}else if(note >= 70){
+    note2 = "C";
+}else if(note >= 60){
+    note2 = "D";
+}else if(note >= 50){
+    note2 = "E";
+}else{
+    note2 = "F";
+}
+ 
+console.log(note2);
