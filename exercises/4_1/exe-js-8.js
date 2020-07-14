502ea2cc360034a5781d50e9ab2cd5ed12ad3491
Anderson R <misterregisslv@gmail.com>
2020-07-14 01:27:54 -0300

    adicionado exe 8 vericar se um numero é par e retornar false ou true

---
+++ exercises/4_1/exe-js-8.js
@@ -0,0 +1,11 @@
+let num1 = 5;
+let num2 = 7;
+let num3 = 9;
+
+let resultado = false;
+
+if((num1 % 2 == 0) || (num2 % 2 == 0) || (num3 % 2 == 0)){
+    resultado = true;
+}
+
+console.log(resultado);
