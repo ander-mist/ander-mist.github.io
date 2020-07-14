df89d69af3f4db83a08eae7a21477e3b99babe07
Anderson R <misterregisslv@gmail.com>
2020-07-14 18:13:39 -0300

    adicionado exercicio 3 media do vetor

---
+++ exercises/4.2/exercicio-03.js
@@ -0,0 +1,10 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media = 0.0;
+
+for(let i = 0; i < numbers.length; i += 1){
+    soma += numbers[i];
+}
+
+media = soma / numbers.length;
+console.log("a media do vetor é: " + media);
