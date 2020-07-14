6ee811a48369c496ba1e2937e18adf0c975d1c07
Anderson R <misterregisslv@gmail.com>
2020-07-14 13:17:28 -0300

    adicionado exe 9 vericar se um numero é impar e retornar false ou true

---
+++ exercises/4_1/exe-js-9.js
@@ -0,0 +1,10 @@
+let num1 = 5;
+let num2 = 6;
+let num3 = 10;
+let resultado = false;
+
+if((num1 % 2 != 0) || (num2 % 2 != 0) || (num3 % 2 != 0)){
+    resultado = true;
+}
+
+console.log(resultado);0
