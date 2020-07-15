bf97b1d273a02ea88be6545c4a05e8bab943b872
Anderson R <misterregisslv@gmail.com>
2020-07-15 18:01:56 -0300

    exercicio 02 terminado

---
+++ exercises/4_3/exercicio-02.js
@@ -0,0 +1,9 @@
+//2- Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+let letra =" ";
+let numeroAsterisco = 5;
+
+for(let index = 0; index < numeroAsterisco; index += 1){
+    letra += "*";
+    console.log(letra);
+}
