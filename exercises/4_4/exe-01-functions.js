110497d153cc117f095fb90fea55fa8d33d4d04f
Anderson R <misterregisslv@gmail.com>
2020-07-21 18:16:27 -0300

    exe 01 functions feito

--- exercises/4_4/exe-01-functions.js
+++ exercises/4_4/exe-01-functions.js
@@ -1,26 +1,31 @@
 
 
-function validarPalindromo(palavra){
-    let inversa = "";
-    for(index = palavra.length-1; index >= 0; index -= 1){
-        inversa += palavra[index];
-    }
-    if(inversa == palavra){
-        return true + " = "+ palavra +" /"+ inversa;
-    }else{
-        return false + " != " + palavra +" /"+ inversa;
-    }
+// function validarPalindromo(palavra){
+//     let inversa = "";
+//     for(index = palavra.length-1; index >= 0; index -= 1){
+//         inversa += palavra[index];
+//     }
+//     if(inversa == palavra){
+//         return true + " = "+ palavra +" /"+ inversa;
+//     }else{
+//         return false + " != " + palavra +" /"+ inversa;
+//     }
     
-}
-
-console.log(validarPalindromo("radar"));
-
-
-// let a = "aba";
-// let b = "aba";
-
-// if(a == b){
-//     console.log(true);
-// }else{
-//     console.log(false);
 // }
+
+// console.log(validarPalindromo("radar"));
+
+
+function verificaPalindrome(palavra) {
+    let arrayLetras = palavra.split("");
+    let isPalindrome = true;
+    for (let index in arrayLetras) {
+      if (arrayLetras[index] != palavra[(palavra.length - 1) - index]) {
+        isPalindrome = false;
+      }
+    }
+    return isPalindrome;
+  }
+
+  console.log(verificaPalindrome('arara')); //true
+console.log(verificaPalindrome('caramujo'));
