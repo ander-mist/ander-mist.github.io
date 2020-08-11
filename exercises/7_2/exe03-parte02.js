29ab4c718a3552b89ef1647e6042dd8ba71d5a54
Anderson R <misterregisslv@gmail.com>
2020-08-11 19:03:27 -0300

    exercicio3 parte 2

---
+++ exercises/7_2/exe03-parte02.js
@@ -0,0 +1,32 @@
+// Crie uma função para mostrar o tamanho de um objeto.
+
+const lesson1 = {
+    materia: 'Matemática',
+    numeroEstudantes: 20,
+    professor: 'Maria Clara',
+    turno: 'manhã',
+  };
+  
+  const lesson2 = {
+    materia: 'História',
+    numeroEstudantes: 20,
+    professor: 'Carlos',
+  };
+  
+  const lesson3 = {
+    materia: 'Matemática',
+    numeroEstudantes: 10,
+    professor: 'Maria Clara',
+    turno: 'noite',
+  };
+
+  const tamanhoObjeto = (objeto) => {
+      const obj = Object.keys(objeto);
+      let NumObjetos = 0;
+      for (i in obj) {
+        NumObjetos += 1;
+      }
+     console.log(NumObjetos);
+  }
+
+  tamanhoObjeto(lesson3);
