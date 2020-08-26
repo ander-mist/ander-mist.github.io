11275256a79132df3a5f74ef062645c7fcc15091
Anderson R <misterregisslv@gmail.com>
2020-08-26 18:44:53 -0300

    adicionada a função sendMarsTemperature com setTimeout

--- exercises/9_1/exercicio1-9-1.js
+++ exercises/9_1/exercicio1-9-1.js
@@ -10,3 +10,17 @@ const getPlanet = () => {
   };
   
   setTimeout(() => getPlanet(), 4000); // imprime Marte depois de 4 segundos
+
+  const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = (callback) => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+// crie a função sendMarsTemperature abaixo
+const sendMarsTemperature = function () {
+    setTimeout(() => console.log('Mars temperature is: 30 degree Celsius'),5000) 
+}
+
+sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
